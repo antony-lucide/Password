4f8960f69027dc405fbaf1d824339876d0f9cecb
antony-lucide <antony.lucide@laplateforme.io>
2023-02-06 12:06:10 +0100

    Ceci est le rendu final du projet generateur de mot de passe en python, pour la pool.

--- Password.py
+++ Password.py
@@ -1,24 +1,19 @@
 import random
 import string
-
-#Créer une nouvelle fenêtre sur linux ou window/mac
-#Variable pour stocker ma chaines de charactères
-#Variable pour stocker la taille de ma chaines de charactères
-#J'ai définies la valeur de ma taille à 8 par défaut
+import adafruit_hashlib as hashlib
 
 
-#Fonction permettant de générer un mot de passe
 def generation():
     #Variable qui stock les charactères
     #Et la Variable qui sera le résultat
     #Avec la boucle pour la longeur dans ma chaines de charactères
+    #L'addiction de ma variable résultat avec la librarie random afin de choisir au hasard des charactères
+    #L'import du string permet de raccourcir les chaines de charactère qui sont deja prédefinis grâce à cette librairie
     equation = string.ascii_letters + string.ascii_lowercase + string.ascii_uppercase
-        #L'addiction de ma variable résultat avec la librarie random afin de choisir au hasard des charactères
-        #et changer une variable en string, grâce à la variable déclaré en dehors de ma fonction qui était déja une string, pour définir le type de variable de mon résultat
     mot_de_passe = "".join(random.choice(equation) for i in range(8))
     return(mot_de_passe)
 
-
+#Condition permettant de vérifier si l'input donner par l'utiliseur est valide, en vérifiant la majuscule,la minuscule, et le chiffre
 def Manuellement(mot_de_passe):
     if len(mot_de_passe) != 8:
             return False
@@ -31,16 +26,22 @@ def Manuellement(mot_de_passe):
     else:
         return True
 
-
+#Je déclare d'abord que la variable "mdp", est l'instance de ma fonction generation
+#Ensuite j'encode une variable qui me permettra  d'encoder mon mot de passe crypté entré par l'user et un mot de passe suggérer
+#Ensuite je suis une condition pour vérisier si l'user respecte les Majuscule, minuscule, et chiffre
+#Si les conditions sont réspecter alors je print le mot de passe génerer cripter
+#Si les conditions sont pas réspecter alors je lui suggère un mot de passe valide
 mdp = generation()
-
+Password = hashlib.sha256(mdp.encode('utf-8')).hexdigest()
 while True:
     user = input("Rentrer un MDP: ")
-    if Manuellement(mot_de_passe=user):
+    user = hashlib.sha256(mdp.encode('utf-8')).hexdigest()
+    if Manuellement(user):
         print("Mot de passe valide")
+        print("Mot de passe cripté: ", user)
         break
     else:
         print("Mot de passe incorrect")
         mdp = generation()
-        print(f"Mot de passe suggérer: {mdp}")
+        print(f"Mot de passe suggérer: {Password}")
         
